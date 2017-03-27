34d9d6884001d7b2e5d283a70eb85ad64eb8f932
Igor <igorbaesse86@gmail.com>
2017-03-27 13:29:12 -0300

    Site cartorio

---
+++ PortalDesktop.Jaguarao/PortalDesktop/Controllers/JustificativaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalDesktop.Controllers
+{
+    public class JustificativaController : Controller
+    {
+        // GET: Justificativa
+        public ActionResult Redirecionar()
+        {
+            return View("Justificativa");
+        }
+    }
+}
---
+++ PortalDesktop.Jaguarao/PortalDesktop/Models/JustificativadePonto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalDesktop.Models
+{
+    public class JustificativadePonto
+    {
+        public DateTime dataocorrencia { get; set; }
+        public string colaborador { get; set; }
+        public string setor { get; set; }
+        public string motivo { get; set; }
+        public string jsutificativa { get; set; }
+        public string abonado { get; set; }
+        public string jsutificativadoabono { get; set; }
+        public string caminhoarquivo { get; set; }
+
+    }
+}
